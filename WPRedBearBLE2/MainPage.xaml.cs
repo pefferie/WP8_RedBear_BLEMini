e2c0783787cafe89bafcffe3c6f4f6ed4c6ce8f5
Iouri Simernitski <pefferie@hotmail.com>
2015-03-20 08:58:42 -0700

    Support RX

--- WPRedBearBLE2/MainPage.xaml.cs
+++ WPRedBearBLE2/MainPage.xaml.cs
@@ -1,23 +1,22 @@
-﻿using System;
+﻿using Microsoft.Phone.Controls;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
-using WPRedBearBLE2.Resources;
-using Windows.Devices.Enumeration;
-using Windows.Devices.Bluetooth.GenericAttributeProfile;
-using Windows.Devices.Bluetooth;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
 
 //tested on the Nokia Lumia 920 and the Lumia 521 both running the Windows Phone 8.1 Cyan update
 //RedBear BLE Mini board was connected directly to a CP2102 USB UART and communication was tested using CoolTerm http://freeware.the-meiers.org/
 //Try http://www.themethodology.net for more information
+
+// tested on Lumia 1520 and RedBear BLE Shield
 
 namespace WPRedBearBLE2
 {
@@ -28,7 +27,8 @@ namespace WPRedBearBLE2
         BluetoothLEDevice _device;
         IReadOnlyList<GattDeviceService> _services;
         GattDeviceService _service;
-        GattCharacteristic _characteristic;
+        GattCharacteristic _characteristicTX;
+        GattCharacteristic _characteristicRX;
 
         // Constructor
         public MainPage()
@@ -40,13 +40,13 @@ namespace WPRedBearBLE2
             btnSendMessage.Click += btnSendMessage_Click;
         }
 
-        void btnSendMessage_Click(object sender, RoutedEventArgs e)
+        async void btnSendMessage_Click(object sender, RoutedEventArgs e)
         {
-            SendMessage(txtMessage.Text);
+            await SendMessage(txtMessage.Text);
             txtMessage.Text = String.Empty;
         }
 
-        async Task RedBearConnect()
+        async void RedBearConnect()
         {
             if (await RefreshDeviceList() == 0)
                 return;
@@ -57,7 +57,7 @@ namespace WPRedBearBLE2
             if (FindService() == false)
                 return;
 
-            if (FindCharacteristic() == false)
+            if (await FindCharacteristic() == false)
                 return;
 
             txtMessage.IsEnabled = true;
@@ -92,7 +92,7 @@ namespace WPRedBearBLE2
             {
                 for (int i = 0; i < bleDevices.Count; i++)
                 {
-                    if (bleDevices[i].Name == "Biscuit" || bleDevices[i].Name == "BLE Mini")
+                    if (bleDevices[i].Name == "Biscuit" || bleDevices[i].Name == "BLE Mini" || bleDevices[i].Name == "BlendMicro" || bleDevices[i].Name == "BLE Shield")
                     {
                         _device = await BluetoothLEDevice.FromIdAsync(bleDevices[i].Id);
                         _services = _device.GattServices;
@@ -128,18 +128,49 @@ namespace WPRedBearBLE2
             return false;
         }
 
-        bool FindCharacteristic()
+        async Task<bool> FindCharacteristic()
         {
+            bool result = false;
             foreach (var c in _service.GetCharacteristics(new Guid("713d0003-503e-4c75-ba94-3148f18d941e")))
             {
                 //"unauthorized access" without proper permissions
-                _characteristic = c;
+                _characteristicTX = c;
+
                 OutputMessage("Found characteristic: " + c.Uuid);
-                return true;
+                result = true;
+                break;
             }
 
-            OutputMessage("Could not find characteristic or permissions are incorrrect");
-            return false;
+            foreach (var c in _service.GetCharacteristics(new Guid("713d0002-503e-4c75-ba94-3148f18d941e")))
+            {
+                //"unauthorized access" without proper permissions
+                _characteristicRX = c;
+                foreach (var i in c.GetDescriptors(new Guid("00002902-0000-1000-8000-00805f9b34fb")))
+                {
+                    try
+                    {
+                        _characteristicRX.ValueChanged += OnDataReceived;
+                        var newDesc = GattClientCharacteristicConfigurationDescriptorValue.Notify;
+                        await _characteristicRX.WriteClientCharacteristicConfigurationDescriptorAsync(newDesc);
+                    }
+                    catch (Exception ex)
+                    {
+                        OutputMessage("Exception: " + ex);
+                    }
+                }
+
+                OutputMessage("Found characteristic: " + c.Uuid);
+                result = true;
+                break;
+            }
+
+
+            if (!result)
+            {
+                OutputMessage("Could not find characteristic or permissions are incorrrect");
+            }
+
+            return result;
         }
 
         async Task<bool> SendMessage(string msg)
@@ -151,8 +182,8 @@ namespace WPRedBearBLE2
 
             try
             {
-                await _characteristic.WriteValueAsync(buffer, GattWriteOption.WriteWithoutResponse);
-
+                var result = await _characteristicTX.WriteValueAsync(buffer, GattWriteOption.WriteWithoutResponse);
+                Debug.Assert(result == GattCommunicationStatus.Success);
                 OutputMessage("Sent message: " + msg);
             }
             catch (Exception ex)
@@ -163,11 +194,53 @@ namespace WPRedBearBLE2
             return true;
         }
 
+        private void OnDataReceived(GattCharacteristic sender, GattValueChangedEventArgs args)
+        {
+            var bytes = args.CharacteristicValue.ToArray();
+
+            Dispatcher.BeginInvoke(() =>
+                {
+                    OutputMessage("r: " + FormatMessage(bytes));
+                });
+        }
+
+        private static string FormatMessage(byte[] bytes)
+        {
+            var builder = new StringBuilder();
+
+            var allAScii = bytes.All(c => IsAscii(c));
+
+            if (allAScii)
+            {
+                return Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+            }
+                // The BLEControllerSketch echoes back a one-letter cmmand
+                // followed by a byte array of raw data. We want 
+                // to special-case this situation
+            else if (IsAscii(bytes[0]))
+            {
+                builder.Append((char)bytes[0]);
+                builder.Append("(0x" + bytes[0].ToString("x") + ")");
+
+                var result = String.Join(", ", new string[] { builder.ToString() }.Concat(bytes.Skip(1).Select(i => "0x" + i)));
+
+                return result;
+            }
+            else
+            {
+                return String.Join(", ", bytes.Select(i => "0x" + i));
+            }
+        }
+
+        private static bool IsAscii(byte c)
+        {
+            return c >= 0x20 && c <= 0x7F;
+        }
+
         void OutputMessage(string msg)
         {
             Debug.WriteLine(msg);
             txtOutput.Text += msg + "\r\n";
         }
-
     }
 }
